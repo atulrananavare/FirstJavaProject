ddb9b0766b78e298e57e25895b58459ed1bda390
atul rananavare <rananavareatul07@gmail.com>
2021-06-05 10:51:27 +0530

    Added additional commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,5 +4,11 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println(100);
+
+        m1(10,50);
+    }
+
+    public static void m1(int a, int b){
+        System.out.println(a+b);
     }
 }
